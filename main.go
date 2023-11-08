c6f4aa605975faa8a4ff1074c3401631d8976eb5
Rumit Ayvovich Vokitra <sergilak280498@mail.ru>
2023-11-08 16:36:21 +0300

    2

--- main.go
+++ main.go
@@ -1,72 +1,24 @@
 package main
 
 import (
-	"C"
 	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB
-
 func main() {
-	var err error
-	db, err = sql.Open("sqlite3", "mydb.db")
+	db, err := sql.Open("sqlite3", "store.db")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer db.Close()
-
-	// Создайте таблицу, если она не существует
-	createTable()
-
-	// Создайте экземпляр Gin
-	r := gin.Default()
-
-	// Обработчик для вывода "Hello, World!" на главной странице
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, World!")
-	})
-
-	// Обработчик для получения данных из базы данных
-	r.GET("/data", getData)
-
-	// Запустите веб-сервер на порту 8080
-	r.Run(":8080")
-}
-
-func createTable() {
-	createTableSQL := `
-        CREATE TABLE IF NOT EXISTS data (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT
-        );
-    `
-	_, err := db.Exec(createTableSQL)
+	result, err := db.Exec("insert into products (model, company, price) values ('iPhone X', $1, $2)",
+		"Apple", 72000)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-}
+	fmt.Println(result.LastInsertId()) // id последнего добавленного объекта
+	fmt.Println(result.RowsAffected()) // количество добавленных строк
 
-func getData(c *gin.Context) {
-	rows, err := db.Query("SELECT id, name FROM data")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	var data string
-	for rows.Next() {
-		err := rows.Scan(&data)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(data)
-	}
-
-	c.JSON(http.StatusOK, gin.H{"data": data})
 }
