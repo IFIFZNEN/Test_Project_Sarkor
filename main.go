0432d56f5ee2898a2a0c4fb6639d042ce7b85f8a
Rumit Ayvovich Vokitra <sergilak280498@mail.ru>
2023-11-09 14:48:43 +0300

    2

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"database/sql"
+	"net/http"
+	"strconv"
 
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
+
+// var jwtKey = []byte("password") // Секретный ключ
+
+type Credentials struct {
+	Password string `json:"password"`
+	Login    string `json:"login"`
+}
+
+type Claims struct {
+	Login string `json:"login"`
+	jwt.StandardClaims
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./test.db")
@@ -14,30 +32,107 @@ func main() {
 	}
 	defer db.Close()
 
-	// Созданиче SQL таблицы
-	sql_table := `
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        login VARCHAR NOT NULL,
-        password VARCHAR NOT NULL,
-        name VARCHAR NOT NULL,
-        age INTEGER
-    );`
-
-	_, err = db.Exec(sql_table)
-	if err != nil {
-		panic(err)
-	}
-
-	// Запуск Джин
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world, Я Артиков Темур Улугбекович, 25 лет, из Узбекистана",
-		})
+	// РЕГИСТРАЦИЯ!
+	r.POST("/user/register", func(c *gin.Context) {
+		login := c.PostForm("login")
+		password := c.PostForm("password")
+		name := c.PostForm("name")
+		ageStr := c.PostForm("age")
+
+		// Convert age from string to int
+		age, err := strconv.Atoi(ageStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Недопустимое значение возраста!"})
+			return
+		}
+
+		// Hashing the password
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Не удалось хешировать пароль!"})
+			return
+		}
+
+		// Preparing SQL statement
+		stmt, err := db.Prepare("INSERT INTO users(login, password, name, age) VALUES(?, ?, ?, ?)")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Не удалось подготовить SQL запрос!"})
+			return
+		}
+		defer stmt.Close()
+
+		// Executing SQL statement
+		_, err = stmt.Exec(login, hashedPassword, name, age)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Не удалось выполнить SQL запрос!"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Регистрация пользователя успешно завершена!"}) // для проверки требуется запустить консоль в записать следующую команду: "curl -d "login=user&password=pass&name=John&age=30" -X POST http://localhost:8080/user/register"
+
 	})
 
-	// Запуск тут
-	r.Run() // Дефолтные значения :8080
+	// АВТОРИЗАЦИЯ!
+	r.POST("/user/auth", func(c *gin.Context) {
+		var creds Credentials
+		// Получаем данные из JSON
+		if err := c.ShouldBindJSON(&creds); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+			return
+		}
+
+		// Ищем пользователя в базе данных
+		var hashedPassword string
+		var id int
+		err := db.QueryRow("SELECT id, password FROM users WHERE login = ?", creds.Login).Scan(&id, &hashedPassword)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				// Если пользователь не найден, возвращаем ошибку
+				c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid login credentials"})
+				return
+			}
+			// Если возникла другая ошибка при запросе к БД, возвращаем ошибку сервера
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Server error"})
+			return
+		}
+
+		// Сравниваем предоставленный пароль с сохраненным хешем
+		if err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password)); err != nil {
+			// Если пароли не совпадают, возвращаем ошибку
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid login credentials"})
+			return
+		}
+
+		// Создаем новый токен для пользователя
+		expirationTime := time.Now().Add(5 * time.Minute)
+		claims := &Claims{
+			Login: creds.Login,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: expirationTime.Unix(),
+				Id:        string(id),
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create token"})
+			return
+		}
+
+		// Устанавливаем токен в куки
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:    "SESSTOKEN",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
+
+		c.JSON(http.StatusOK, gin.H{"message": "Logged in"})
+	})
+
+	// Define other routes and handlers
+
+	r.Run() // By default it will listen on :8080
 }
---
+++ secret_key.go
@@ -0,0 +1,3 @@
+package main
+
+var jwtKey = []byte("password") // Секретный ключ
